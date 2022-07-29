b124f77c09dfc1c0b6947e872d6d3b86a945afc6
Marco Moscatelli <marco.moscatelli06@gmail.com>
2022-07-29 12:58:42 +0200

    fdsafsdafsad

--- routes/orders.js
+++ routes/orders.js
@@ -444,17 +444,25 @@ router.get('/checkout/buynow/:id', async (req, res) => {
 
   let passed = false;
   for(item of product.sizes){
+    console.log('passed')
     if(item.size == size){
+      console.log('sizeeuqal')
       if(item.remaining > 0){
+        console.log('not remaining')
         passed = true;
       }
     }
   }
 
   if(!passed){
+    console.log('not passed')
+    console.log(product)
+    console.log(req.query)
     req.flash('error', 'Invalid size')
     return res.redirect(`/product/${product.urlSlug}`)
   }
+
+  console.log('---------------------------')
 
   const key = process.env.STRIPE_KEY;
 
